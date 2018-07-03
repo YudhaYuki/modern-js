a2c0df5764b5191b0ca5601f699cf722893778db
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-03 18:05:54 +0200

    Debugging Our Application

--- 6-todo-app/todo-functions.js
+++ 6-todo-app/todo-functions.js
@@ -24,13 +24,17 @@ const renderTodos = function(todos, filters){
     const filteredTodos = todos.filter(function (todo) {
         const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
         const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
-
+        debugger;
         return searchTextMatch && hideCompletedMatch;
     });
+
+    debugger;
 
     const incompleteTodos = filteredTodos.filter(function (todo) {
         return !todo.completed
     });
+
+    debugger;    
 
     document.querySelector('#todos').innerHTML = '';
     
