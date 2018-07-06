441ff49b5b876a0fba4183f9390f7237b1d4fe2f
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-06 11:36:05 +0200

    PROTOTYPE - Use SPLIT method

--- 8-oop/1-person.js
+++ 8-oop/1-person.js
@@ -4,8 +4,23 @@ const Person = function (firstName, lastName, age) {
     this.age = age;
 }
 
-const me = new Person('Yudha', 'Yuki', 27);
-console.log(me);
+Person.prototype.getBio = function() {
+    return `${this.firstName} is ${this.age}.`;
+}
+
+Person.prototype.location = 'Indonesia';
+
+Person.prototype.setName = function(fullName) {
+    const names = fullName.split(' ');
+    this.firstName = names[0];
+    this.lastName = names[1];
+}
+
+const me = new Person('Yudha', 'Yuki', 36);
+me.setName('Alexis Turner');
+// console.log(me.location);
+console.log(me.getBio());
+
 
 const personTwo = new Person('Rika', 'Yuki', 27);
-console.log(personTwo);
+console.log(personTwo.getBio());
