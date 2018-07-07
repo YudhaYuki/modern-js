6bebfed0b21a862dfdf292e13a223512bcd09e99
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-07 12:25:20 +0200

    Getters and Setters - Example 3 for STUDENT and EMPLOYEE in PERSON file

--- 8-oop/1-person.js
+++ 8-oop/1-person.js
@@ -18,10 +18,14 @@ class Person {
         return bio;
     }
 
-    setName(fullName) {
+    set fullName(fullName) {
         const names = fullName.split(' ');
         this.firstName = names[0];
         this.lastName = names[1];
+    }
+
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
     }
 
 }
@@ -33,33 +37,13 @@ class Employee extends Person {
     }
 
     getBio() {
-        return `${this.firstName} ${this.lastName} is a ${this.position}.`
+        return `${this.fullName} is a ${this.position}.`
     }
 
     getYearsLeft() {
         return 65 - this.age
     }
 }
-
-
-// const me = new Employee('Yudha', 'Yuki', 27, 'teacher', ['Learning', 'Cooking']);
-// me.setName('Alexis Turner');
-// console.log(me.getBio());
-// console.log(me.getYearsLeft());
-
-// const personTwo = new Person('Rika', 'Yuki', 36);
-// console.log(personTwo.getBio());
-
-
-// 1. Create class for students
-// 2. Track student grad. 0 - 100
-// 3. Override bio to print a passsing of failing message . 70 and above "Yudha us passing"
-// 4. Create "updateGrade", that takes the amount to add or remove from grade
-
-// Create student 
-// Print status (failing or passing)
-// Change grade to change status
-// Print status again
 
 class Student extends Person{
     constructor(firstName, lastName, age, grade, likes) {
@@ -77,7 +61,13 @@ class Student extends Person{
     }
 }
 
-const me = new Student('Yudha', 'Yuki', 27, 71, []);
-console.log(me.getBio());
-me.updateGrade(-5);
+// const me = new Student('Yudha', 'Yuki', 27, 71, []);
+// me.fullName = 'Rika Hermawanti';
+// console.log(me.getBio());
+// me.updateGrade(-5);
+// console.log(me.getBio());
+
+
+const me = new Employee('Yudha', 'Yuki', 27, 'Teacher', []);
+me.fullName = 'Rika Hermawanti';
 console.log(me.getBio());
