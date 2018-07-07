a21f75d0ac2a08df2fb7d9bb60848779e7efc0f1
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-07 09:55:31 +0200

    Using CLASS SYNTAX in person.js instead of CONSTRUCTOR alone

--- 8-oop/1-person.js
+++ 8-oop/1-person.js
@@ -1,45 +1,35 @@
 // Prototypal Iheritance
 
-const Person = function (firstName, lastName, age, likes = []) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.likes = likes;
-}
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.likes = likes;
+    }
 
-Person.prototype.getBio = function() {
-    let bio = `${this.firstName} is ${this.age}.`;
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`;
+        
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`;
+        });
+    
+        return bio;
+    }
 
-    this.likes.forEach((like) => {
-        bio += ` ${this.firstName} likes ${like}.`;
-    });
+    setName(fullName) {
+        const names = fullName.split(' ');
+        this.firstName = names[0];
+        this.lastName = names[1];
+    }
 
-    return bio;
-}
-
-// Person.prototype.location = 'Indonesia';
-
-Person.prototype.setName = function(fullName) {
-    const names = fullName.split(' ');
-    this.firstName = names[0];
-    this.lastName = names[1];
 }
 
 const me = new Person('Yudha', 'Yuki', 27, ['Learning', 'Cooking']);
-
-// me.getBio = function () {
-//     return 'This is fake!';
-// }
-
 me.setName('Alexis Turner');
-// console.log(me.location);
 console.log(me.getBio());
 
 
 const personTwo = new Person('Rika', 'Yuki', 36);
-
-// Person.prototype.getBio = function () {
-//     return 'Testing Testing';
-// }
-
 console.log(personTwo.getBio());
