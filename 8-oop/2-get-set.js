07d531df660c4be153096c6e23a6aa4fed48912c
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-07 12:17:23 +0200

    Getters and Setters - Example 2 with ARRAY

--- 8-oop/2-get-set.js
+++ 8-oop/2-get-set.js
@@ -1,12 +1,15 @@
 const data = {
+    locations: [],
     get location() {
         return this._location;
     },
     set location(value) {
         this._location = value.trim();
+        this.locations.push(this._location);
     }
 }
 
 // Code that uses the data object
 data.location = '    Brussels       ';
-console.log(data.location); 
+data.location = 'Leiden';
+console.log(data); 
