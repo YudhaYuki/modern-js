9625715c8c69d15582383df0c794145deca86b90
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-07 11:21:39 +0200

    Create subclass Employee from Person

--- 8-oop/1-person.js
+++ 8-oop/1-person.js
@@ -26,10 +26,26 @@ class Person {
 
 }
 
-const me = new Person('Yudha', 'Yuki', 27, ['Learning', 'Cooking']);
+class Employee extends Person {
+    constructor(firstName, lastName, age, position, likes) {
+        super(firstName, lastName, age, likes);
+        this.position = position;
+    }
+
+    getBio() {
+        return `${this.firstName} ${this.lastName} is a ${this.position}.`
+    }
+
+    getYearsLeft() {
+        return 65 - this.age
+    }
+}
+
+
+const me = new Employee('Yudha', 'Yuki', 27, 'teacher', ['Learning', 'Cooking']);
 me.setName('Alexis Turner');
 console.log(me.getBio());
-
+console.log(me.getYearsLeft());
 
 const personTwo = new Person('Rika', 'Yuki', 36);
 console.log(personTwo.getBio());
