2ee76a56c7f47cd936eec7086674dfdf7b8cb6b9
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-07 09:14:22 +0200

    4 additional functionalities HANGMAN Games

--- 9-hangman/hangman.js
+++ 9-hangman/hangman.js
@@ -1,13 +1,53 @@
-// 1. Display the puzzle to the browser instead of console
-// 2. Display the guesses left to the browser instead of console
-// 3. Separate the Hangman definition from the rest of the app code (use app.js)
+// 1. Setup new "status" property with initial value of "playing"
+// 2. Create method for recalculating status to "playing", "finish", or "failed"
+// 3. Call that method after a guess is processed
+// 4. Use console.log to print the status
+
+// Start the game and see "playing"
+// Make two inccorect guesses to see "failed"
+// Refresh the browser and guess "c", "a", and "t" to see "finished"
+
+
 
 
 const Hangman = function (word, remainingGuesses) {
     this.word = word.toLowerCase().split('');
     this.remainingGuesses = remainingGuesses;
     this.guessedLetters = [];
+    this.status = 'playing';
 };
+
+Hangman.prototype.calculateStatus = function () {
+    
+    const finished = this.word.every((letter) => {
+        return this.guessedLetters.includes(letter);
+    });
+
+
+    // const lettersUnguessed = this.word.filter((letter) => {
+    //     return !this.guessedLetters.includes(letter);
+    // });
+    // const finished = lettersUnguessed.length === 0;
+
+
+    // let finished = true;
+
+    // this.word.forEach((letter) => {
+    //     if (this.guessesLetters.includes(letter)) {
+            
+    //     } else {
+    //         finished = false;
+    //     }
+    // });
+
+    if (this.remainingGuesses === 0) {
+        this.status = 'failed';
+    } else if (finished) {
+        this.status = 'finished';
+    } else {
+        this.status = 'playing';
+    }
+}
 
 Hangman.prototype.getPuzzle = function () {
     let puzzle = '';
@@ -35,5 +75,7 @@ Hangman.prototype.makeGuess = function (guess) {
     if (isUnique && isBadGuess) {
         this.remainingGuesses = this.remainingGuesses - 1;
     }
+
+    this.calculateStatus();
 }
 
