9751f40a764a153a13bef8a120303617fae23e64
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-07 16:15:11 +0200

    HTTP Request from JavaScript

--- 9-hangman/app.js
+++ 9-hangman/app.js
@@ -11,4 +11,20 @@ window.addEventListener('keypress', function (e) {
     game1.makeGuess(guess);
     puzzleEl.textContent = game1.puzzle;
     guessesEl.textContent = game1.statusMessage;   
+});
+
+
+
+// Making an HTTP request
+
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', function(e){
+    if (e.target.readyState === 4) {
+        const data = JSON.parse(e.target.responseText);
+        console.log(data);
+    };
 })
+
+request.open('GET', 'http://puzzle.mead.io/puzzle');
+request.send();
