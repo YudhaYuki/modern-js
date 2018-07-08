f451e0a3248155f8b57f4160b82043dcc2d8d3da
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-08 09:32:33 +0200

    SYNC and ASYNC for COUNTRY

--- 9-hangman/app.js
+++ 9-hangman/app.js
@@ -13,7 +13,7 @@ window.addEventListener('keypress', (e) => {
     guessesEl.textContent = game1.statusMessage;   
 });
 
-getPuzzle( (error, puzzle) => {
+getPuzzle("1", (error, puzzle) => {
     if (error) {
         console.log(`Error: ${error}`)
     } else {
@@ -21,43 +21,19 @@ getPuzzle( (error, puzzle) => {
     }
 });
 
+getCountry("ID", (error, country) => {
+    if (error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(`Country name: ${country.name}`)
+    }
+});
+
 
 
 // Making an HTTP request
 
-// const request = new XMLHttpRequest();
-
-// request.addEventListener('readystatechange', (e) => {
-//     if (e.target.readyState === 4 && e.target.status === 200) {
-//         const data = JSON.parse(e.target.responseText);
-//         console.log(data);
-//     } else if (e.target.readyState === 4) {
-//         console.log('An error has take place');
-//     };
-// });
-
-// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3');
-// request.send();
-
-
-// 1. Make a new request for all countries
-// 2. Parse the responseText to get back the array of objects
-// 3. Find your country object by it's country code (alpha2Code property)
-// 4. Print the full country name (name property)
-
-
-// const countryCode = "ID";
-// const countryRequest = new XMLHttpRequest();
-
-// countryRequest.addEventListener('readystatechange', (e) => {
-//     if (e.target.readyState === 4 && e.target.status === 200) {
-//         const data = JSON.parse(e.target.responseText);
-//         const country = data.find((country) => country.alpha2Code === countryCode);
-//         console.log(country.name);
-//     } else if (e.target.readyState === 4) {
-//         console.log('Unable to fetch data');
-//     }
-// });
-
-// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
-// countryRequest.send();
+// 1. Create a new function for getting country details
+// 2. Call it with two arguments: country code, the callback function
+// 3. Make the HTTP request and call the callback with country information
+// 4. Use the callback to print the country name
