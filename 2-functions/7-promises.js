102d2a731aa43dafe24dd697c2de3947aaa4b451
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-08 14:33:03 +0200

    Exploring Promises - When it is successful

---
+++ 2-functions/7-promises.js
@@ -0,0 +1,28 @@
+// Callback
+const getDataCallback = (callback) => {
+    setTimeout(() => {
+        callback(undefined, 'this is the data')
+    }, 2000);
+}
+
+getDataCallback((err, data) => {
+    if (err) {
+
+    } else {
+        console.log(data)
+    }
+});
+
+
+
+// Promise
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('This is the promise data');
+    }, 2000)
+})
+
+myPromise.then((data) => {
+    console.log(data);
+})
+
