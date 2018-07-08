deb91f973e1ba1afbc1ed6956e2c3b58e0e44bb1
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-08 08:43:46 +0200

    Create new file for putting request function, and just call it back in app file

--- 9-hangman/app.js
+++ 9-hangman/app.js
@@ -13,23 +13,29 @@ window.addEventListener('keypress', (e) => {
     guessesEl.textContent = game1.statusMessage;   
 });
 
-
+getPuzzle( (error, puzzle) => {
+    if (error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(puzzle);
+    }
+});
 
 // Making an HTTP request
 
-const request = new XMLHttpRequest();
+// const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText);
-        console.log(data);
-    } else if (e.target.readyState === 4) {
-        console.log('An error has take place');
-    };
-});
+// request.addEventListener('readystatechange', (e) => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText);
+//         console.log(data);
+//     } else if (e.target.readyState === 4) {
+//         console.log('An error has take place');
+//     };
+// });
 
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3');
-request.send();
+// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3');
+// request.send();
 
 
 // 1. Make a new request for all countries
@@ -38,18 +44,18 @@ request.send();
 // 4. Print the full country name (name property)
 
 
-const countryCode = "ID";
-const countryRequest = new XMLHttpRequest();
+// const countryCode = "ID";
+// const countryRequest = new XMLHttpRequest();
 
-countryRequest.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText);
-        const country = data.find((country) => country.alpha2Code === countryCode);
-        console.log(country.name);
-    } else if (e.target.readyState === 4) {
-        console.log('Unable to fetch data');
-    }
-});
+// countryRequest.addEventListener('readystatechange', (e) => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText);
+//         const country = data.find((country) => country.alpha2Code === countryCode);
+//         console.log(country.name);
+//     } else if (e.target.readyState === 4) {
+//         console.log('Unable to fetch data');
+//     }
+// });
 
-countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
-countryRequest.send();
+// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
+// countryRequest.send();
