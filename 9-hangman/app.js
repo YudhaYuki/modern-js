aaa5f16926688f27556dcf5db01c358055b9049b
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-09 18:30:58 +0200

    Hangman Game: Create INIT function and reset button to start the game

--- 9-hangman/app.js
+++ 9-hangman/app.js
@@ -1,33 +1,40 @@
 const puzzleEl = document.querySelector('#puzzle');
 const guessesEl = document.querySelector('#guesses');
-const game1 = new Hangman('Car Parks', 2);
+let game1;
 
-puzzleEl.textContent = game1.puzzle;
-guessesEl.textContent = game1.statusMessage;
+// puzzleEl.textContent = game1.puzzle;
+// guessesEl.textContent = game1.statusMessage;
 
-window.addEventListener('keypress', (e) => {
-    const guess = String.fromCharCode(e.charCode)
-    game1.makeGuess(guess);
+// window.addEventListener('keypress', (e) => {
+//     const guess = String.fromCharCode(e.charCode)
+//     game1.makeGuess(guess);
+//     puzzleEl.textContent = game1.puzzle;
+//     guessesEl.textContent = game1.statusMessage;   
+// });
+
+const render = () => {
     puzzleEl.textContent = game1.puzzle;
     guessesEl.textContent = game1.statusMessage;   
-});
+}
 
-getPuzzle('2').then((puzzle) => {
-    console.log(puzzle);
-}).catch((err) => {
-    console.log(`Error: ${err}`);
-});
+const startGame = async () => {
+    const puzzle = await getPuzzle('2');
+    game1 = new Hangman(puzzle, 5);
+    render();
+};
 
-getCurrentCountry().then((country) => {
-    console.log(country.name);
-}).catch((error) => {
-    console.log(error)
-})
+document.querySelector("#reset").addEventListener('click', startGame);
 
-// getLocation().then((location) => {
-//     return getCountry(location.country);
-// }).then((country) => {
-//     console.log(country.name);
+startGame();
+
+// getPuzzle('2').then((puzzle) => {
+//     console.log(puzzle);
 // }).catch((err) => {
-//     console.log(`Error: ${err}`)
+//     console.log(`Error: ${err}`);
 // });
+
+// getCurrentCountry().then((country) => {
+//     console.log(country.name);
+// }).catch((error) => {
+//     console.log(error)
+// })
