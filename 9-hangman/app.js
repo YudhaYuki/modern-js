9ccba8e028d0b18a9883a0cea1b640760e6de46b
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-09 15:49:51 +0200

    Fetch API - Puzzle

--- 9-hangman/app.js
+++ 9-hangman/app.js
@@ -5,7 +5,6 @@ const game1 = new Hangman('Car Parks', 2);
 puzzleEl.textContent = game1.puzzle;
 guessesEl.textContent = game1.statusMessage;
 
-
 window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess);
@@ -13,18 +12,29 @@ window.addEventListener('keypress', (e) => {
     guessesEl.textContent = game1.statusMessage;   
 });
 
-getPuzzle('1').then((puzzle) => {
+getPuzzle('2').then((puzzle) => {
     console.log(puzzle);
-}, (err) => {
+}).catch((err) => {
     console.log(`Error: ${err}`);
 });
 
 
-// 1. Covert getCountry to return a new promise
-// 2. Call getCountry and use then to pritn country name or the error
+// getCountry('ID').then((country) => {
+//     console.log(country.name)
+// }, (err) => {
+//     console.log(`Error: ${err}`)
+// });
 
-getCountry('ID').then((country) => {
-    console.log(country.name)
-}, (err) => {
-    console.log(`Error: ${err}`)
-});
+
+
+// fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
+//     if  (response.status === 200) {
+//         return response.json();
+//     } else {
+//         throw new Error('Unable to fetch the puzzle');
+//     }
+// }).then((data) => {
+//     console.log(data.puzzle)
+// }).catch((error) => {
+//     console.log(error);
+// });
