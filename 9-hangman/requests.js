d52f4cfb0909308fd72ac9d33f1ca816d4b0ec3b
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-09 17:48:13 +0200

    Convert getCountry and getLocation into ASYNC and AWAIT

--- 9-hangman/requests.js
+++ 9-hangman/requests.js
@@ -10,38 +10,30 @@ const getPuzzle = async (wordCount) => {
 }
 
 
-const getPuzzleOld = (wordCount) => {
-    return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
-        if (response.status === 200) {
-            return response.json();
-        } else {
-            throw new Error('Unable to fetch puzzle');
-        }
-    }).then((data) => {
-        return data.puzzle
-    });
+// 1. Convert getCountry to an async function that uses await
+// 2. Convert getLocation to an async function that uses await
+
+
+const getCountry = async (countryCode) => {
+    const response = await fetch('http://restcountries.eu/rest/v2/all');
+
+    if (response.status === 200) {
+        const data = await response.json();
+        return data.find((country) => country.alpha2Code === countryCode);
+    } else {
+        throw new Error('Unable to fetch the country');
+    }
 }
 
 
 
-const getCountry = (countryCode) => {
-    return fetch('http://restcountries.eu/rest/v2/all').then((response) => {
-        if(response.status === 200) {
-            return response.json();
-        } else {
-            throw new Error('Unable to fetch data');
-        }
-    }).then((data) => {
-        return data.find((country) => country.alpha2Code === countryCode);
-    });
-};
 
-const getLocation = () => {
-    return fetch('http://ipinfo.io/json?token=4cf9307feeb14b').then((response) => {
-        if(response.status === 200) {
-            return response.json();
-        } else {
-            throw new Error('Unable to fetch location');
-        }
-    })
+const getLocation = async () => {
+    const response = await fetch('http://ipinfo.io/json?token=4cf9307feeb14b');
+
+    if (response.status === 200) {
+        return response.json();
+    } else {
+        throw new Error('Unable to fetch the country');
+    }
 }
