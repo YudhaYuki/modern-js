d34d6ecdcdc7d0b7985b2f0c5c0077acf5c98a17
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-09 16:49:28 +0200

    get your current location (Promise Challenge 2 - Make the country code BE into Belgium)

--- 9-hangman/app.js
+++ 9-hangman/app.js
@@ -18,19 +18,21 @@ getPuzzle('2').then((puzzle) => {
     console.log(`Error: ${err}`);
 });
 
-getCountry('ID').then((country) => {
-    console.log(country.name)
-}).catch((err) => {
-    console.log(`Error: ${err}`)
-});
+// getCountry('ID').then((country) => {
+//     console.log(country.name)
+// }).catch((err) => {
+//     console.log(`Error: ${err}`)
+// });
 
 // 1. Create getLocation function which takes no argument
 // 2. Setup getLocation to make a request to the url and parse the data
 // 3. Use getLocation to print the city, region, and country information
 
 getLocation().then((location) => {
-    console.log(`You are currently in ${location.city} ${location.region} ${location.country}!`)
-    console.log(location);
+    // console.log(`You are currently in ${location.city} ${location.region} ${location.country}!`)
+    return getCountry(location.country);
+}).then((country) => {
+    console.log(country.name);
 }).catch((err) => {
     console.log(`Error: ${err}`)
 });
