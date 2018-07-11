9456e7ad3a65963fd5514067287a1721e14cdc78
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-11 14:52:30 +0200

    Modify output file name for NOTES_APP WEBPACK

--- 12-notes-app/webpack.config.js
+++ 12-notes-app/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     output: {
         path:path.resolve(__dirname, 'public/scripts'),
-        filename: 'bundle.js'
+        filename: '[name]-bundle.js'
     },
     module: {
         rules: [{
