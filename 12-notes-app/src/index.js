53b0d711956f8ee4f27c4999f82b7d20ca9ea57a
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-11 15:47:00 +0200

    Coverting Notes App: PART III - Remove Function

--- 12-notes-app/src/index.js
+++ 12-notes-app/src/index.js
@@ -1,5 +1,6 @@
-import { getNotes, createNote } from './notes';
+import { getNotes, createNote, removeNote } from './notes';
 
-console.log(getNotes());
-createNote();
+// console.log(getNotes());
+// createNote();
+removeNote('26f0f139-b5f0-4918-9ff8-13e75a3adacd')
 console.log(getNotes());
--- 12-notes-app/src/notes.js
+++ 12-notes-app/src/notes.js
@@ -36,6 +36,16 @@ const createNote = () => {
    saveNotes();
 }
 
+// Remove a note from the list
+const removeNote = (id) => {
+    const noteIndex = notes.findIndex((note) => note.id === id);
+
+    if (noteIndex > -1) {
+        notes.splice(noteIndex, 1);
+        saveNotes();
+    }
+}
+
 notes = loadNotes();
 
-export { getNotes, createNote };
+export { getNotes, createNote, removeNote };
