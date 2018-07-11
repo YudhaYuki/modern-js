2b96489453c8b82cb1eb1c5b9760a862385e85db
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-11 09:50:01 +0200

    Name and default export EXAMPLE

--- 10-boilerplate/src/index.js
+++ 10-boilerplate/src/index.js
@@ -1,15 +1,9 @@
 // import './utilities';
-import { add, name } from './utilities';
+import otherSquare, { add, name } from './utilities';
 import { scream } from './scream';
 
 console.log('index.js');
 console.log(add(32, 1));
 console.log(name);
 console.log(scream(name));
-
-
-// 1. Create a new file called scream.js
-// 2. Export a "scream" function that takes a string
-//      a. Covert that string to uppercase
-//      b. Add "!" onto the end
-// 3. Import scream into index.js and use it
+console.log(otherSquare(10));
--- 10-boilerplate/src/utilities.js
+++ 10-boilerplate/src/utilities.js
@@ -1,5 +1,9 @@
-console.log('utilities.js');
+// Named export
+// Default Export
 
 export const add = (a, b) => a + b;
-
 export const name = 'Yudha';
+
+
+const square = (x) => x * x;
+export default square;
